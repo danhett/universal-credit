dc04938eee9a93b003af94249e01489160b4b3d6
Dan Hett <hellodanhett@gmail.com>
2020-04-19 19:13:05 +0100

    Added loads of names for testing

--- js/Main.js
+++ js/Main.js
@@ -2,13 +2,13 @@ particlesJS.load('particlesholder', 'particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
 
-const storytext = document.querySelector('.storytext')
-const storyfx = new TextScramble(storytext)
-const storygrammar = tracery.createGrammar(test);
-
 const headertext = document.querySelector('.headertext')
 const headerfx = new TextScramble(headertext)
-const headergrammar = tracery.createGrammar(phrases);
+const headergrammar = tracery.createGrammar(nameGrammar);
+
+const storytext = document.querySelector('.storytext')
+const storyfx = new TextScramble(storytext)
+const storygrammar = tracery.createGrammar(story);
 
 // create the grammar and first text update
 
